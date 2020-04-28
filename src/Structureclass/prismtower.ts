14d6ae5a708d16a82fa9b4d1f0207377d00f160c
122460 <601028413@qq.com>
2020-04-28 08:44:03 +0800

    处理

--- src/Structureclass/prismtower.ts
+++ src/Structureclass/prismtower.ts
@@ -54,6 +54,8 @@ export class Prismtower extends Structure {
             //
         } else {
             console.log(distance, this.firerange)
+            // clearTimeout(this.firetimer);
+            //this.firetimer =  null
         }
     }
     //攻击
@@ -64,58 +66,77 @@ export class Prismtower extends Structure {
                 this.xrayAttack()
                 clearTimeout(this.firetimer);
                 this.firetimer = null
-            }, 600)
+            }, 2000)
         }
 
     }
     //射线攻击
     xrayAttack() {
-
+        this.clearXray();
         ptattack_audio.playAudio()
-        this.attacktarget.bloodLess(this.harm)
+        //   this.attacktarget.bloodLess(this.harm)
     }
     //绘制_清除射线
     clearXray() {
+        debugger
         let index = 0;
-        let raylength = this.pointDistance(this.positions,this.attacktarget.currentclickpoints,true),
-            verticee2s={
-                x:this.attacktarget.currentclickpoints.x - this.positions.x,
-                y:this.attacktarget.currentclickpoints.y - this.positions.y
+        let raylength = this.pointDistance(this.positions, this.attacktarget.currentclickpoints, true),
+            verticee2s = {
+                x: this.attacktarget.currentclickpoints.x - (this.positions.x + this.size.x + 0.5),
+                y: this.attacktarget.currentclickpoints.y - (this.positions.y + 20)
             },
-            sintheta = verticee2s.x/(this.squalCaculator(verticee2s,true))?verticee2s.x/(this.squalCaculator(verticee2s,true)):0,
-            costheta =  verticee2s.y/(this.squalCaculator(verticee2s,true))?verticee2s.x/(this.squalCaculator(verticee2s,true)):0,
-            sixquoterx = sintheta*raylength/6,
-            sinthetay = costheta*raylength/6;
-        let timerss = setInterval(() => {
-            if (index < 6) {
-                this.ctx.moveTo(this.positions.x+this.size.x*0.5, this.positions.y+10);       //设置起点状态
-                this.ctx.lineTo(this.positions.x+index*sixquoterx, this.positions.y+index*sinthetay);       //设置末端状态
-                this.ctx.lineWidth = 2;          //设置线宽状态
-                this.ctx.strokeStyle = "rgb(191,191,255)";  //设置线的颜色状态
-                this.ctx.stroke();     //进行绘制
-            }
+            sintheta = verticee2s.x / (this.squalCaculator(verticee2s, true)) ? verticee2s.x / (this.squalCaculator(verticee2s, true)) : 0,
+            costheta = verticee2s.y / (this.squalCaculator(verticee2s, true)) ? verticee2s.y / (this.squalCaculator(verticee2s, true)) : 0,
+            sixquoterx = sintheta * raylength / 6,
+            sinthetay = costheta * raylength / 6;
+        this.ctx.beginPath()
+        this.ctx.moveTo(this.positions.x + this.size.x * 0.5, this.positions.y + 20);       //设置起点状态
+        this.ctx.lineTo(this.attacktarget.currentclickpoints.x,this.attacktarget.currentclickpoints.y);       //设置末端状态
+        this.ctx.lineWidth = 1.5;          //设置线宽状态
+        this.ctx.strokeStyle = "rgb(191,191,255)";  //设置线的颜色状态
+        this.ctx.stroke();     //进行绘制
+        let x = Math.min(this.positions.x + this.size.x * 0.5, this.attacktarget.currentclickpoints.x),
+            y = Math.min(this.positions.y + 20, this.attacktarget.currentclickpoints.y),
+            w = Math.abs(this.positions.x + this.size.x * 0.5 - this.attacktarget.currentclickpoints.x),
+            h = Math.abs(this.positions.y + 20 - this.attacktarget.currentclickpoints.y);
+            setTimeout(()=>{
+                this.ctx.clearRect(x, y, w, h)
+            },100)
+        
+        // let timerss = setInterval(() => {
+        //     console.log(timerss)
+        //     if (index < 6) {
 
-            if (index >= 12) {
-                clearInterval(timerss);
-                this.clearLine(sintheta,costheta);
-            }
-            index++
-        }, 16.6)
+        //     }
+
+        //     if (index >= 12) {
+        //         clearInterval(timerss);
+        //        this.clearLine(sintheta,costheta);
+        //     }
+        //     index++
+        // }, 16.6)
     }
-    clearLine(sintheta,costheta) {
+    clearLine(sintheta, costheta) {
 
-       let  raylength = this.pointDistance(this.positions,this.attacktarget.currentclickpoints,true),
-            retanclewidth = (2 / sintheta)?(2 / sintheta):0,
-            retancleheight = (2 * 2 / costheta)?(2 * 2 / costheta):0,
-            counst = 600 / retanclewidth,
+
+        return
+        console.log('cao ')
+        let raylength = this.pointDistance({ x: this.positions.x + this.size.x * 0.5, y: this.positions.y + 20 }, this.attacktarget.currentclickpoints, true),
+            retanclewidth = (1.5 / sintheta) ? (1.5 / sintheta) : 0,
+            retancleheight = (2 * 1.5 / costheta) ? (2 * 1.5 / costheta) : 0,
+            counst = raylength / (1.5 / (sintheta * costheta)),
+            x1 = this.positions.x + this.size.x * 0.5,
+            y1 = this.positions.y + 20,
+            x2 = this.attacktarget.currentclickpoints.x,
+            y2 = this.attacktarget.currentclickpoints.y,
 
             cleartimes = 0;
-        for (let j = 0; j < counst; j++) {
-            let x = retanclewidth * cleartimes + 100,
-                y = (((sintheta/costheta)?sintheta/costheta:0)*x)+ - (0.25) * retancleheight,
+        for (let j = 0; j < Math.abs(counst); j++) {
+            let x = (this.positions.x + this.size.x) + cleartimes * retanclewidth,
+                y = (((sintheta / costheta) ? sintheta / costheta : 0) * x) + (x1 * y2 - x2 * y1) / (x1 - x2),
                 width = retanclewidth,
                 height = retancleheight
-            this.ctx.clearRect(x, y, width, height);
+            this.ctx.clearRect(x, y, Math.abs(width), Math.abs(height));
             cleartimes++;
         }
     }
